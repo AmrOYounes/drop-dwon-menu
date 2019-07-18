e65f8e3660a82a9fde763fcf10d014b676b55c07
AmrOYounes <amr.younis@stu.najah.edu>
2019-07-18 03:29:56 +0300

    first react

---
+++ src/components/tourList/tourList.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import Tour from '../tour/tour';
+import './tourlist.scss';
+import { tourData } from '../../tourData';
+
+
+export default class tourList extends Component {
+    state = {
+        tours: tourData
+    };
+    removeTour = id => {
+        const { tours } = this.state;
+        const newtours = tours.filter(tour => tour.id !== id);
+        this.setState({
+            tours: newtours
+        });
+
+    }
+
+    render() {
+        // console.log(this.state.tours);
+        const { tours } = this.state;
+        return (
+            <section className="tourlist">
+                {
+                    tours.map(tour => (<Tour key={tour.id} tour={tour} removeTour={this.removeTour}></Tour>))
+                }
+
+            </section>
+        )
+    }
+}

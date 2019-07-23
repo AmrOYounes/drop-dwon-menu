624f37c6bc0a45859f15d29ec7992a6cd3793ffa
AmrOYounes <amr.younis@stu.najah.edu>
2019-07-23 16:06:14 +0300

    drop some files

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import React from 'react';
 
 import './App.scss';
 import Navbar from './components/Navbar/navbar';
-import TourList from './components/tourList/tourList'
 import { BrowserRouter, Route } from "react-router-dom";
-import Home from './Home';
-import About from './About';
-import tourLists from './components/tourList/tourList';
-import Dropdown from './components/dropdown/dropdown';
+
 
 function App() {
   return (
--- src/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Home() {
-    return (
-        <div>
-            <h1>Hello from Home page</h1>
-        </div>
-    )
-}
